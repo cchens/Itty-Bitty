b2eb1ba9dce8460f4f096ca071194ccbf6973771
Eugene Cheung <ey.cheung@outlook.com>
2015-02-16 18:44:16 -0500

    Misc minor tweaks

--- config/local.js
+++ config/local.js
@@ -80,7 +80,7 @@ module.exports = {
    * 'development' environment.                                              *
    ***************************************************************************/
 
-  // environment: process.env.NODE_ENV || 'development',
+  environment: process.env.NODE_ENV || 'development'
 
   // adapters: {
   //   'default': 'postgres',
