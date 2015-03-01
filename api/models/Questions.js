6ac2c7a9d4e316dfd00590c2614503dc08cce400
Christina Chen <xtina.chen@mail.utoronto.ca>
2015-03-01 11:52:10 -0500

    Questions model

---
+++ api/models/Questions.js
@@ -0,0 +1,57 @@
+/**
+* Questions.js
+*
+* @description :: A Question
+* @docs        :: http://sailsjs.org/#!/documentation/concepts/ORM
+*/
+
+module.exports = {
+
+  attributes: {
+
+    question_id: {
+      type: 'integer',
+      autoIncrement: true,
+      primaryKey: true
+    },
+
+    level_id: {
+      type: 'integer',
+      unique: true
+      notNull: true
+      model: 'Levels'
+    },
+
+    question: {
+      type: 'string',
+      notNull: true
+    }
+
+    choice1: {
+      type: 'string'
+    }
+
+    choice2: {
+      type: 'string'
+    }
+
+    choice3: {
+      type: 'string'
+    }
+
+    choice4: {
+      type: 'string'
+    }
+
+    answer: {
+      type: 'string',
+      notNull: true
+    }
+
+    explanation: {
+      type: 'string',
+      notNull: true
+    }
+
+  }
+};
