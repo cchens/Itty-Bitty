c783676492de5c8670d4e537e42d5639c79acf83
Eugene Cheung <ey.cheung@outlook.com>
2015-03-05 11:11:47 -0500

    Work on merging in Ben's work to quiz page

--- api/controllers/QuestionsController.js
+++ api/controllers/QuestionsController.js
@@ -7,31 +7,32 @@
 module.exports = {
 
   index: function (req, res) {
-    var type  = req.params.type,
-        level = req.params.level;
+    var type      = req.params.type,
+        level_num = req.params.level;
 
     // get appropriate level content from db
     // e.g. http://localhost:1337/tutorials/bitwise/1
-    if (level && type) {
+    if (level_num && type) {
       // checks for the tutoral content by checking level id
       // this will need to be changed so checks for types too
       Levels
-      .find({ level_num: level, type: type })
-      .exec(function (err, levels) {
-        if (levels === undefined) return res.notFound();
+      .findOne({ type: type, level_num: level_num })
+      .exec(function (err, level) {
+        if (level === undefined) return res.notFound();
         if (err) return res.negotiate(err);
 
         // Checks for the question data by level
         Questions
-        .find({ level_id: level })
+        .find({ level_id: level.level_id })
         .exec(function (err, questions) {
           if (questions === undefined) return res.notFound();
           if (err) return res.negotiate(err);
 
+          var title = "Level " + level_num + " (" + type + ")";
+
           res.view('quiz', {
-            'type': type,
-            'level': level,
-            'tutorial': levels,
+            'title': title,
+            'tutorial': level,
             'questions': questions
           });
         });
