d5757ceda3f762d585a6a1eefb013d4d1ba2869d
Eugene Cheung <ey.cheung@outlook.com>
2015-03-04 22:44:35 -0500

    Remove hard-coded myconf, formatting

--- api/controllers/QuestionsController.js
+++ api/controllers/QuestionsController.js
@@ -4,34 +4,30 @@
  * Description
  */
 
- module.exports = {
+module.exports = {
 
-
-
-  	index: function (req,res) {
-    	var type  = req.params.type,
+  index: function (req,res) {
+    var type  = req.params.type,
         level = req.params.level;
 
-    	// get appropriate level content from db
-    	// currently it is only based on level of question
-    	// e.g. http://localhost:1337/quiz/1
+    // get appropriate level content from db
+    // currently it is only based on level of question
+    // e.g. http://localhost:1337/quiz/1
 
-    	if(level){
-    		    Questions.find({level_id:level}).exec(function(err, questions) {
+    if (level) {
+      Questions
+      .find({ level_id: level })
+      .exec(function (err, questions) {
+        if (questions === undefined) return res.notFound();
+        if (err) return res.negotiate(err);
 
-                if (questions === undefined) return res.notFound();
-
-                if (err) return res.negotiate(err);
-
-				res.view('quiz', {
-	      			'type': type,
-				    'level': level,
-				    'questions': questions
-				});
-            });
-    	}
-
-	    
+        res.view('quiz', {
+          'type': type,
+          'level': level,
+          'questions': questions
+        });
+      });
+    }
   }
 
 }
