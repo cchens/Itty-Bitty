e97dc7c112413d3ea529570b7e40415c8ff93802
Eugene Cheung <ey.cheung@outlook.com>
2015-03-22 12:44:29 -0400

    Formatting, remove todo from Answer.js

--- api/models/Answer.js
+++ api/models/Answer.js
@@ -1,61 +1,45 @@
-
-function checkSolution (passport, next) {
-     todo: 'Not implemented yet!'
-     
-      // donno y this doesn't run
-      if(answer.mcAnswer){
-        /*
-        // donno y this doesn't run
-
-        Questions
-            .find()
-            .where({ question_id:answer.question_id })
-            .exec(function (err, questions) {
-              if (questions === undefined) return res.notFound();
-              if (err) return res.negotiate(err); 
-              if (answer == questions.answer) {
-                return true;
-              }
-              else{
-                return false;
-              }
-            }
-            */
-    
-    }
-     else if(answer.strAnswer){
-
-     }
-}
-
-
 var Answer = {
+
   attributes: {
-      question_id: {
-        type: 'integer',
-        autoIncrement: true,
-        primaryKey: true
-      },
+    question_id: {
+      type: 'integer',
+      autoIncrement: true,
+      primaryKey: true
+    },
 
-      level_id: {
-        model: 'Levels'
-      },
+    level_id: {
+      model: 'Levels'
+    },
 
-      mcAnswer: {
-        type: 'string',
-        defaultTo: null
-      },
+    mcAnswer: {
+      type: 'string',
+      defaultTo: null
+    },
 
-      strAnswer: {
-        type: 'string',
-        notNull: true
-      }
-  },
-    validateAnswer: function (answer, next) {
-      checkSolution(answer, next);
+    strAnswer: {
+      type: 'string',
+      notNull: true
     }
+  },
 
-  
+  validateAnswer: function (answer, next) {
+
+    if (answer.mcAnswer) {
+      Questions
+      .find()
+      .where({ question_id: answer.question_id })
+      .exec(function (err, questions) {
+        if (questions === undefined) return res.notFound();
+        if (err) return res.negotiate(err);
+
+        return answer === questions.answer;
+      }
+
+    }
+    else if (answer.strAnswer) {
+
+    }
+  }
 };
 
 module.exports = Answer;
