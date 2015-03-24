690ad22a1cf2dcd48a38a84a00d7b027dd97f4bb
xinrwu <xinr.wu@live.com>
2015-03-24 13:55:02 -0400

    basic achievements table

--- api/controllers/AchievementsController.js
+++ api/controllers/AchievementsController.js
@@ -6,6 +6,17 @@
  */
 
 module.exports = {
-	
+	index: function (req, res) {
+		Achievements
+      	.find()
+      	.sort('name ASC')
+      	.exec(function (err, achievements) {
+        	if (achievements === undefined) return res.notFound();
+        	if (err) return res.negotiate(err);
+        	sails.log.info(achievements[0].name)
+    		res.view('achievements', {
+    			'achievements': achievements
+    		});
+    	});
+  	},
 };
-
