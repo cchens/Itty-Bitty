f7492b9dcfbe4e1ffb1333f400b76463bac58f24
Eugene Cheung <ey.cheung@outlook.com>
2015-03-24 14:07:23 -0400

    Throw achievements table into user profile

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -35,8 +35,17 @@ module.exports = {
       if (scores === undefined) return res.notFound();
       if (err) return res.negotiate(err);
 
-      res.view('profile', {
-        'scores': scores
+      Achievements
+      .find()
+      .sort('name ASC')
+      .exec(function (err, achievements) {
+        if (achievements === undefined) return res.notFound();
+        if (err) return res.negotiate(err);
+
+        res.view('profile', {
+          'scores': scores,
+          'achievements': achievements
+        });
       });
     });
   },
