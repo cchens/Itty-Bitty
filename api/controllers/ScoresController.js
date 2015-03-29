4aee179ac185690057152c2af8b9a3a4e158db7f
leebang <leebang31698@gmail.com>
2015-03-29 00:20:57 -0400

    Displaying users with their total score, let eric do the ranking?

--- api/controllers/ScoresController.js
+++ api/controllers/ScoresController.js
@@ -10,16 +10,23 @@ module.exports = {
   leaderboards: function (req, res) {
     Scores
     .find()
-    .sort({ user: 1 })
+    .sort( 'user ASC' )
     .exec(function (err, scores) {
       if (scores === undefined) return res.notFound();
       if (err) return res.negotiate(err);
 
-      res.view('leaderboards', {
-        'scores': scores
+      User
+      .find()
+      .sort('id ASC')
+      .exec(function (err, user) {
+        if (user === undefined) return res.notFound();
+        if (err) return res.negotiate(err);
+
+        res.view('leaderboards', {
+          'scores': scores,
+          'user' : user
+        });
       });
     });
   },
-
 };
-
